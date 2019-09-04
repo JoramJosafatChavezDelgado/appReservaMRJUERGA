45233c18ee9195bf4bc076958a4eb1893e58cef4
joramcito <joramjosafatchavezdelgado@gmail.com>
2019-09-04 09:13:54 -0500

    agregando productos y muestra

---
+++ app/src/main/java/com/chavez/my_application/mostrarproductos.java
@@ -0,0 +1,14 @@
+package com.chavez.my_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mostrarproductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrarproductos);
+    }
+}
